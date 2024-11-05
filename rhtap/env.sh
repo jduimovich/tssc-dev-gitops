97d65a6c6aa46db541f9cc026b75fd7546b67507
John Duimovich <jduimovich@gmail.com>
2024-11-05 12:46:58 -0500

    Testing in CI

--- rhtap/env.sh
+++ rhtap/env.sh
@@ -48,4 +48,4 @@ export STRICT=${STRICT-true}
 export EFFECTIVE_TIME=${EFFECTIVE_TIME-now}
 export HOMEDIR=${HOMEDIR-$(pwd)}
 export TUF_MIRROR="${MY_TUF_MIRROR:-http://tuf.rhtap.svc}"
-# Update forced CI test Tue Nov  5 12:36:40 EST 2024
+# Update forced CI test Tue Nov  5 12:46:57 EST 2024
