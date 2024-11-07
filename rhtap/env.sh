cb4bb28f6d5674a12725865f7952e705eb8c7498
jduimovich <jduimovich@gmail.com>
2024-11-06 20:11:43 -0500

    Testing in CI

--- rhtap/env.sh
+++ rhtap/env.sh
@@ -41,11 +41,11 @@ export TARGET_BRANCH=${TARGET_BRANCH-""}
 # enterprise contract
 export POLICY_CONFIGURATION=${POLICY_CONFIGURATION-"github.com/enterprise-contract/config//rhtap-jenkins"}
 #internal, assumes jenkins is local openshift
-export REKOR_HOST="${MY_REKOR_HOST:-http://rekor-server.rhtap.svc}"
+export REKOR_HOST="none"
 export IGNORE_REKOR=${IGNORE_REKOR-false}
 export INFO=${INFO-true}
 export STRICT=${STRICT-true}
 export EFFECTIVE_TIME=${EFFECTIVE_TIME-now}
 export HOMEDIR=${HOMEDIR-$(pwd)}
-export TUF_MIRROR="${MY_TUF_MIRROR:-http://tuf.rhtap.svc}"
-# Update forced CI test Wed Nov  6 19:46:04 EST 2024
+export REKOR_HOST="none"
+# Update forced CI test Wed Nov  6 20:11:42 EST 2024
