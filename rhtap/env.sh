f0b374c88e6c2b8b76408ca52f164e44d3e0089f
John Duimovich <jduimovich@gmail.com>
2024-11-11 21:53:54 -0500

    Testing in CI

--- rhtap/env.sh
+++ rhtap/env.sh
@@ -46,6 +46,6 @@ export INFO=${INFO-true}
 export STRICT=${STRICT-true}
 export EFFECTIVE_TIME=${EFFECTIVE_TIME-now}
 export HOMEDIR=${HOMEDIR-$(pwd)}
-export REKOR_HOST=https://rekor-server-rhtap-tas.apps.rosa.fqbre-nx5qu-gm2.x1ie.p3.openshiftapps.com
-export TUF_MIRROR=https://tuf-rhtap-tas.apps.rosa.fqbre-nx5qu-gm2.x1ie.p3.openshiftapps.com
-# Update forced CI test Mon Nov 11 15:02:53 EST 2024
+export REKOR_HOST=https://rekor-server-rhtap-tas.apps.rosa.a57h9-d7pqw-97x.daq5.p3.openshiftapps.com
+export TUF_MIRROR=https://tuf-rhtap-tas.apps.rosa.a57h9-d7pqw-97x.daq5.p3.openshiftapps.com
+# Update forced CI test Mon Nov 11 21:53:51 EST 2024
