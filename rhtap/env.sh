2f3a8cd7fd528b2fc98d29dc1f309588b9f09e98
John Duimovich <jduimovich@gmail.com>
2024-11-20 19:59:50 -0500

    Testing in CI

--- rhtap/env.sh
+++ rhtap/env.sh
@@ -53,4 +53,4 @@ export SBOMS_DIR=results/sboms
 export REKOR_HOST=
 export IGNORE_REKOR=true
 export TUF_MIRROR=
-# Update forced CI test Wed Nov 20 19:36:48 EST 2024
+# Update forced CI test Wed Nov 20 19:59:33 EST 2024
